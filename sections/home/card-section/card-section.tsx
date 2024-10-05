9e3dda96c8c541a79f9dffeb1f83a76a81f5d66e
the-coder-o <abdulbositsharipovv@gmail.com>
2024-10-05 11:04:13 +0500

    chnaged some styles from home page

--- sections/home/card-section/card-section.tsx
+++ sections/home/card-section/card-section.tsx
@@ -19,47 +19,51 @@ export const CardSection = () => {
           <Input type="search" placeholder="Search portfolios" className="w-[250px] rounded-xl" />
         </div>
       </div>
-      <div className="grid grid-cols-4 gap-6">
-        <Link className="group h-full overflow-hidden rounded-3xl border border-gray-200 bg-gray-100 antialiased dark:border-neutral-800 dark:bg-neutral-900" href="">
-          <div className="relative overflow-hidden rounded-2xl transition duration-200 group-hover:shadow-xl">
-            <Image
-              alt="Component Thumbnail"
-              loading="lazy"
-              width="720"
-              height="500"
-              decoding="async"
-              className="aspect-video h-[14rem] rounded-2xl bg-cover object-cover object-top blur-0 transition duration-300 group-hover:scale-105"
-              src="https://www.portfolioshub.com/_next/image?url=https%3A%2F%2Fspotted-swordfish-236.convex.site%2FgetImage%3FstorageId%3Dkg2f3v613gp8nt71f8jmpf1z7x6zkgry&w=640&q=75"
-              style={{ color: 'transparent' }}
-            />
-          </div>
-          <div className="mt-4 flex flex-col items-start p-3">
-            <div className="flex w-full justify-between">
-              <div className="mb-2 flex items-center">
-                <p className="text-base font-medium text-neutral-700 dark:text-neutral-100">Hero Sections</p>
+      <div className="grid grid-cols-4 gap-6 gap-y-9">
+        {cardData.map((card, index) => (
+          <Link key={index} className="group h-full overflow-hidden rounded-3xl border border-gray-200 bg-gray-100 antialiased dark:border-neutral-800 dark:bg-neutral-900" href="">
+            <div className="relative overflow-hidden rounded-2xl transition duration-200 group-hover:shadow-xl">
+              <Image alt="Component Thumbnail" loading="lazy" width="720" height="500" decoding="async" className="aspect-video h-[14rem] rounded-2xl bg-cover object-cover object-top blur-0 transition duration-300 group-hover:scale-105" src={card.imageUrl} style={{ color: 'transparent' }} />
+            </div>
+            <div className="mt-4 flex flex-col items-start p-3">
+              <div className="flex w-full justify-between">
+                <div className="mb-2 flex items-center">
+                  <p className="text-base font-medium text-neutral-700 dark:text-neutral-100">{card.title}</p>
+                </div>
+                <div className="flex h-[22px] w-[60px] items-center justify-center rounded-full border border-transparent bg-primary text-sm font-semibold text-foreground transition-colors hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 dark:border-green-300 dark:bg-green-700">
+                  {card.priceTag}
+                </div>
               </div>
-              <div className="flex h-[22px] w-[60px] items-center justify-center rounded-full border border-transparent bg-primary text-sm font-semibold text-foreground transition-colors hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 dark:border-green-300 dark:bg-green-700">
-                Free
+              <p className="mt-2 max-w-xs text-sm font-normal text-neutral-500 dark:text-neutral-500">{card.description}</p>
+              <div className="mt-4 flex w-full items-center justify-between">
+                <div className="flex flex-wrap gap-1">
+                  {card.badges.map((badge: string, badgeIndex: number) => (
+                    <Badge key={badgeIndex} variant="outline">
+                      {badge}
+                    </Badge>
+                  ))}
+                </div>
+                <div className="!z-50 flex items-center">
+                  <Button variant="ghost" className="h-7 w-7" size="icon">
+                    <HeartIcon className="h-5 w-5" />
+                  </Button>
+                  <Button variant="ghost" className="h-7 w-7" size="icon">
+                    <ExternalLink className="h-5 w-5" />
+                  </Button>
+                </div>
               </div>
             </div>
-            <p className="mt-2 max-w-xs text-sm font-normal text-neutral-500 dark:text-neutral-500">A collection of hero sections that are modern and stand out</p>
-            <div className="mt-4 flex w-full items-center justify-between">
-              <div className="flex flex-wrap gap-1">
-                <Badge variant="outline">Developer</Badge>
-                <Badge variant="outline">Designer</Badge>
-              </div>
-              <div className="!z-50 flex items-center">
-                <Button variant="ghost" className="h-7 w-7" size="icon">
-                  <HeartIcon className="h-5 w-5" />
-                </Button>
-                <Button variant="ghost" className="h-7 w-7" size="icon">
-                  <ExternalLink className="h-5 w-5" />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </div>
     </>
   )
 }
+
+const cardData = Array(8).fill({
+  title: 'Hero Sections',
+  description: 'A collection of hero sections that are modern and stand out',
+  priceTag: 'Free',
+  imageUrl: 'https://www.portfolioshub.com/_next/image?url=https%3A%2F%2Fspotted-swordfish-236.convex.site%2FgetImage%3FstorageId%3Dkg2f3v613gp8nt71f8jmpf1z7x6zkgry&w=640&q=75',
+  badges: ['Developer', 'Designer'],
+})
