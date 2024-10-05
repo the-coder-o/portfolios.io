6054d1f3622ed562944ef179823df08e9c172973
the-coder-o <abdulbositsharipovv@gmail.com>
2024-10-05 10:34:13 +0500

    finished portfolios card and fixed some styles from home page

--- sections/home/card-section/card-section.tsx
+++ sections/home/card-section/card-section.tsx
@@ -1,7 +1,10 @@
+import { ExternalLink, HeartIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
 export const CardSection = () => {
@@ -17,7 +20,7 @@ export const CardSection = () => {
         </div>
       </div>
       <div className="grid grid-cols-4 gap-6">
-        <Link className="group h-full overflow-hidden rounded-3xl border border-gray-200 bg-gray-100 antialiased dark:border-neutral-800 dark:bg-neutral-900" href="/products/hero-sections">
+        <Link className="group h-full overflow-hidden rounded-3xl border border-gray-200 bg-gray-100 antialiased dark:border-neutral-800 dark:bg-neutral-900" href="">
           <div className="relative overflow-hidden rounded-2xl transition duration-200 group-hover:shadow-xl">
             <Image
               alt="Component Thumbnail"
@@ -25,12 +28,12 @@ export const CardSection = () => {
               width="720"
               height="500"
               decoding="async"
-              className="aspect-video h-[14rem] rounded-2xl object-cover object-top blur-0 transition duration-300 group-hover:scale-105"
-              src="https://pro.aceternity.com/_next/image?url=https%3A%2F%2Fassets.aceternity.com%2Fpro%2Fhero-sections.png&w=1920&q=75"
+              className="aspect-video h-[14rem] rounded-2xl bg-cover object-cover object-top blur-0 transition duration-300 group-hover:scale-105"
+              src="https://www.portfolioshub.com/_next/image?url=https%3A%2F%2Fspotted-swordfish-236.convex.site%2FgetImage%3FstorageId%3Dkg2f3v613gp8nt71f8jmpf1z7x6zkgry&w=640&q=75"
               style={{ color: 'transparent' }}
             />
           </div>
-          <div className="mt-4 flex flex-col items-start px-4 pb-4 pt-2">
+          <div className="mt-4 flex flex-col items-start p-3">
             <div className="flex w-full justify-between">
               <div className="mb-2 flex items-center">
                 <p className="text-base font-medium text-neutral-700 dark:text-neutral-100">Hero Sections</p>
@@ -40,81 +43,20 @@ export const CardSection = () => {
               </div>
             </div>
             <p className="mt-2 max-w-xs text-sm font-normal text-neutral-500 dark:text-neutral-500">A collection of hero sections that are modern and stand out</p>
-          </div>
-        </Link>
-        <Link className="group h-full overflow-hidden rounded-3xl border border-gray-200 bg-gray-100 antialiased dark:border-neutral-800 dark:bg-neutral-900" href="/products/hero-sections">
-          <div className="relative overflow-hidden rounded-2xl transition duration-200 group-hover:shadow-xl">
-            <Image
-              alt="Component Thumbnail"
-              loading="lazy"
-              width="720"
-              height="500"
-              decoding="async"
-              className="aspect-video h-[14rem] rounded-2xl object-cover object-top blur-0 transition duration-300 group-hover:scale-105"
-              src="https://pro.aceternity.com/_next/image?url=https%3A%2F%2Fassets.aceternity.com%2Fpro%2Fhero-sections.png&w=1920&q=75"
-              style={{ color: 'transparent' }}
-            />
-          </div>
-          <div className="mt-4 flex flex-col items-start px-4 pb-4 pt-2">
-            <div className="flex w-full justify-between">
-              <div className="mb-2 flex items-center">
-                <p className="text-base font-medium text-neutral-700 dark:text-neutral-100">Hero Sections</p>
+            <div className="mt-4 flex w-full items-center justify-between">
+              <div className="flex flex-wrap gap-1">
+                <Badge variant="outline">Developer</Badge>
+                <Badge variant="outline">Designer</Badge>
               </div>
-              <div className="flex h-[22px] w-[60px] items-center justify-center rounded-full border border-transparent bg-primary text-sm font-semibold text-foreground transition-colors hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 dark:border-green-300 dark:bg-green-700">
-                Free
+              <div className="!z-50 flex items-center">
+                <Button variant="ghost" className="h-7 w-7" size="icon">
+                  <HeartIcon className="h-5 w-5" />
+                </Button>
+                <Button variant="ghost" className="h-7 w-7" size="icon">
+                  <ExternalLink className="h-5 w-5" />
+                </Button>
               </div>
             </div>
-            <p className="mt-2 max-w-xs text-sm font-normal text-neutral-500 dark:text-neutral-500">A collection of hero sections that are modern and stand out</p>
-          </div>
-        </Link>
-        <Link className="group h-full overflow-hidden rounded-3xl border border-gray-200 bg-gray-100 antialiased dark:border-neutral-800 dark:bg-neutral-900" href="/products/hero-sections">
-          <div className="relative overflow-hidden rounded-2xl transition duration-200 group-hover:shadow-xl">
-            <Image
-              alt="Component Thumbnail"
-              loading="lazy"
-              width="720"
-              height="500"
-              decoding="async"
-              className="aspect-video h-[14rem] rounded-2xl object-cover object-top blur-0 transition duration-300 group-hover:scale-105"
-              src="https://pro.aceternity.com/_next/image?url=https%3A%2F%2Fassets.aceternity.com%2Fpro%2Fhero-sections.png&w=1920&q=75"
-              style={{ color: 'transparent' }}
-            />
-          </div>
-          <div className="mt-4 flex flex-col items-start px-4 pb-4 pt-2">
-            <div className="flex w-full justify-between">
-              <div className="mb-2 flex items-center">
-                <p className="text-base font-medium text-neutral-700 dark:text-neutral-100">Hero Sections</p>
-              </div>
-              <div className="flex h-[22px] w-[60px] items-center justify-center rounded-full border border-transparent bg-primary text-sm font-semibold text-foreground transition-colors hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 dark:border-green-300 dark:bg-green-700">
-                Free
-              </div>
-            </div>
-            <p className="mt-2 max-w-xs text-sm font-normal text-neutral-500 dark:text-neutral-500">A collection of hero sections that are modern and stand out</p>
-          </div>
-        </Link>
-        <Link className="group h-full overflow-hidden rounded-3xl border border-gray-200 bg-gray-100 antialiased dark:border-neutral-800 dark:bg-neutral-900" href="/products/hero-sections">
-          <div className="relative overflow-hidden rounded-2xl transition duration-200 group-hover:shadow-xl">
-            <Image
-              alt="Component Thumbnail"
-              loading="lazy"
-              width="720"
-              height="500"
-              decoding="async"
-              className="aspect-video h-[14rem] rounded-2xl object-cover object-top blur-0 transition duration-300 group-hover:scale-105"
-              src="https://pro.aceternity.com/_next/image?url=https%3A%2F%2Fassets.aceternity.com%2Fpro%2Fhero-sections.png&w=1920&q=75"
-              style={{ color: 'transparent' }}
-            />
-          </div>
-          <div className="mt-4 flex flex-col items-start px-4 pb-4 pt-2">
-            <div className="flex w-full justify-between">
-              <div className="mb-2 flex items-center">
-                <p className="text-base font-medium text-neutral-700 dark:text-neutral-100">Hero Sections</p>
-              </div>
-              <div className="flex h-[22px] w-[60px] items-center justify-center rounded-full border border-transparent bg-primary text-sm font-semibold text-foreground transition-colors hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 dark:border-green-300 dark:bg-green-700">
-                Free
-              </div>
-            </div>
-            <p className="mt-2 max-w-xs text-sm font-normal text-neutral-500 dark:text-neutral-500">A collection of hero sections that are modern and stand out</p>
           </div>
         </Link>
       </div>
