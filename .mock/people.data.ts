32eee9f0f6922a7c5d6bf6e6568607e65404e463
the-coder-o <abdulbositsharipovv@gmail.com>
2024-11-05 18:05:17 +0500

    released new design-ui for home page

---
+++ .mock/people.data.ts
@@ -0,0 +1,11 @@
+export const people = [
+  { id: 1, name: 'The Coder', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/115723431?v=4' },
+  { id: 2, name: 'Shokhjakhon', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/113612600?v=4' },
+  { id: 3, name: 'Nodirbek', designation: 'Backend Developer', image: 'https://avatars.githubusercontent.com/u/129874807?v=4' },
+  { id: 4, name: 'Muhammadqodir', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/114002954?s=100&v=4' },
+  { id: 5, name: 'Shokhjakhon', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/78145953?v=4' },
+  { id: 6, name: 'Ibrohim', designation: 'Frontend Developer', image: 'https://avatars.githubusercontent.com/u/46422014?v=4' },
+  { id: 7, name: 'Azim Pulat', designation: 'Frontend Developer', image: 'https://avatars.githubusercontent.com/u/35038240?v=4' },
+  { id: 8, name: 'Saidov Zohid', designation: 'Backend Developer', image: 'https://avatars.githubusercontent.com/u/116626702?v=4' },
+  { id: 9, name: 'Mukhammadjon', designation: 'Frontend Developer', image: 'https://avatars.githubusercontent.com/u/99827567?v=4' },
+]
