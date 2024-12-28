45f0d3499312b7804849f3ab204e7cf0a2ce2cf1
the-coder-o <abdulbositsharipovv@gmail.com>
2024-12-28 13:00:00 +0500

    updated users

--- .mock/people.data.ts
+++ .mock/people.data.ts
@@ -1,10 +1,10 @@
 export const people = [
-  { id: 1, name: 'The Coder', designation: 'The Founder', image: 'https://avatars.githubusercontent.com/u/115723431?v=4' },
-  { id: 2, name: 'Shokhjakhon', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/113612600?v=4' },
-  { id: 3, name: 'Nodirbek', designation: 'Backend Developer', image: 'https://avatars.githubusercontent.com/u/129874807?v=4' },
-  { id: 4, name: 'Muhammadqodir', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/114002954?s=100&v=4' },
-  { id: 5, name: 'Shokhjakhon', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/78145953?v=4' },
-  { id: 6, name: 'Ibrohim', designation: 'Frontend Developer', image: 'https://avatars.githubusercontent.com/u/46422014?v=4' },
-  { id: 7, name: 'Jontstaz', designation: 'Cryptocurrency', image: 'https://avatars.githubusercontent.com/u/6379873?v=4' },
-  { id: 8, name: 'Saidov Zohid', designation: 'Backend Developer', image: 'https://avatars.githubusercontent.com/u/116626702?v=4' },
+  { id: 1, name: 'Abdul Basit', designation: 'The Founder', image: 'https://avatars.githubusercontent.com/u/115723431?v=4' },
+  { id: 2, name: 'Nuruddin Iminokhunov', designation: 'Ceo of GlobalMove', image: 'https://i.ibb.co/ft04Xvs/photo-2024-12-28-12-37-22.jpg' },
+  { id: 3, name: 'Azim Pulat', designation: 'Software Engineer at Google', image: 'https://azimjon.com/static/assets/images/azimjon.jpg' },
+  { id: 4, name: 'Hosna Qasmei', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/39573679?v=4' },
+  { id: 5, name: 'Jay Kadam', designation: 'Product Designer at Peerlist', image: 'https://dqy38fnwh4fqs.cloudfront.net/UHBAOB6BGGNQPGECB6RAQMJRNA9P/hbaob6bggnqpgecb6raqmjrna9p-profile.webp' },
+  { id: 7, name: 'Sahil Vhora', designation: 'Product designer', image: 'https://dqy38fnwh4fqs.cloudfront.net/UH9O68EGRMD9KE7HDMJALJA7KNNB/h9o68egrmd9ke7hdmjalja7knnb-profile.webp' },
+  { id: 6, name: 'Ramakrushna Mohapatra', designation: 'Data Scientists', image: 'https://media.licdn.com/dms/image/v2/D5603AQFOtnf_XLPMfQ/profile-displayphoto-shrink_200_200/B56ZQA0VYHGQAo-/0/1735180507539?e=1740614400&v=beta&t=ai50ewVkCFcd1Xj4kVm7Nx4s21SMjZuUOz4KgXcrO1U' },
+  { id: 8, name: 'Akash Bhadange', designation: 'Cofounder & CEO of Peerlist', image: 'https://dqy38fnwh4fqs.cloudfront.net/UHDNK79BK6LA89DCMPRQGEGQOGGO/hdnk79bk6la89dcmprqgegqoggo-profile.webp' },
 ]
