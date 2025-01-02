ab556b4cee941e09b21cc7d565c203965dc655a6
The Coder 🧑🏼‍💻 <115723431+the-coder-o@users.noreply.github.com>
2025-01-02 13:31:45 +0500

    Update people.data.ts

--- .mock/people.data.ts
+++ .mock/people.data.ts
@@ -1,6 +1,6 @@
 export const people = [
   { id: 1, name: 'Abdul Basit', designation: 'The Founder', image: 'https://avatars.githubusercontent.com/u/115723431?v=4' },
-  { id: 6, name: 'Adam Powell', designation: 'FullStack Engineer 🇬🇧', image: 'https://instagram.ftas1-1.fna.fbcdn.net/v/t51.2885-19/308677906_174554228454383_115994799837637001_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_ht=instagram.ftas1-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=XdGY68IEqvAQ7kNvgF4PHM1&_nc_gid=f6c71c5e939b47eeb4a88ac2de6e4997&edm=AEYEu-QBAAAA&ccb=7-5&oh=00_AYBqIiNboZAvc1XZdZvJt9VXSpbB3f11Id0jyKKynOgb5A&oe=677C12AE&_nc_sid=ead929' },
+  { id: 6, name: 'Adam Powell', designation: 'FullStack Engineer 🇬🇧', image: 'https://yt3.googleusercontent.com/bsQZFkllgV-YrhWp3D6qTQvji-6ok3uG-zmDi9i-o8UTndoxGge349OVYKlO51QtpoUmydRgXg=s160-c-k-c0x00ffffff-no-rj' },
   { id: 2, name: 'Nuruddin Iminokhunov', designation: 'Ceo of GlobalMove', image: 'https://i.ibb.co/ft04Xvs/photo-2024-12-28-12-37-22.jpg' },
   { id: 3, name: 'Azim Pulat', designation: 'Software Engineer at Google', image: 'https://azimjon.com/static/assets/images/azimjon.jpg' },
   { id: 4, name: 'Hosna Qasmei', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/39573679?v=4' },
