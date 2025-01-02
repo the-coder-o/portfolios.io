ff3ccd158ba0ea4e403a43f127a8e5d185bbae4d
The Coder 🧑🏼‍💻 <115723431+the-coder-o@users.noreply.github.com>
2025-01-02 13:28:52 +0500

    Update people.data.ts

--- .mock/people.data.ts
+++ .mock/people.data.ts
@@ -1,10 +1,10 @@
 export const people = [
   { id: 1, name: 'Abdul Basit', designation: 'The Founder', image: 'https://avatars.githubusercontent.com/u/115723431?v=4' },
+  { id: 6, name: 'Adam Powell', designation: 'FullStack Engineer 🇬🇧', image: 'https://instagram.ftas1-1.fna.fbcdn.net/v/t51.2885-19/308677906_174554228454383_115994799837637001_n.jpg?stp=dst-jpg_s150x150_tt6&_nc_ht=instagram.ftas1-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=XdGY68IEqvAQ7kNvgF4PHM1&_nc_gid=f6c71c5e939b47eeb4a88ac2de6e4997&edm=AEYEu-QBAAAA&ccb=7-5&oh=00_AYBqIiNboZAvc1XZdZvJt9VXSpbB3f11Id0jyKKynOgb5A&oe=677C12AE&_nc_sid=ead929' },
   { id: 2, name: 'Nuruddin Iminokhunov', designation: 'Ceo of GlobalMove', image: 'https://i.ibb.co/ft04Xvs/photo-2024-12-28-12-37-22.jpg' },
   { id: 3, name: 'Azim Pulat', designation: 'Software Engineer at Google', image: 'https://azimjon.com/static/assets/images/azimjon.jpg' },
   { id: 4, name: 'Hosna Qasmei', designation: 'FullStack Developer', image: 'https://avatars.githubusercontent.com/u/39573679?v=4' },
   { id: 5, name: 'Jay Kadam', designation: 'Product Designer at Peerlist', image: 'https://dqy38fnwh4fqs.cloudfront.net/UHBAOB6BGGNQPGECB6RAQMJRNA9P/hbaob6bggnqpgecb6raqmjrna9p-profile.webp' },
   { id: 7, name: 'Sahil Vhora', designation: 'Product designer', image: 'https://dqy38fnwh4fqs.cloudfront.net/UH9O68EGRMD9KE7HDMJALJA7KNNB/h9o68egrmd9ke7hdmjalja7knnb-profile.webp' },
-  { id: 6, name: 'Ramakrushna Mohapatra', designation: 'Data Scientists', image: 'https://media.licdn.com/dms/image/v2/D5603AQFOtnf_XLPMfQ/profile-displayphoto-shrink_200_200/B56ZQA0VYHGQAo-/0/1735180507539?e=1740614400&v=beta&t=ai50ewVkCFcd1Xj4kVm7Nx4s21SMjZuUOz4KgXcrO1U' },
   { id: 8, name: 'Akash Bhadange', designation: 'Cofounder & CEO of Peerlist', image: 'https://dqy38fnwh4fqs.cloudfront.net/UHDNK79BK6LA89DCMPRQGEGQOGGO/hdnk79bk6la89dcmprqgegqoggo-profile.webp' },
 ]
